fb9ef835c50bf654bbf49f3989c7d7986d0d683b
rendrarya <rendrarya12345@gmail.com>
2023-10-11 12:24:55 +0800

    update app py

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("mongodb+srv://test:sparta@cluster0.cyvuwe8.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME =  os.environ.get("DB_NAME")
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.cyvuwe8.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
